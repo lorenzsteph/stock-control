148fe87a8f7aeba4b3f48674a15ccf771652ad82
BK001687 <BK001687@AM-SLORENZI.itmaster.local>
2016-09-19 21:09:32 +0200

    start project

---
+++ StockControl/src/main/java/com/stock/control/configuration/MvcConfiguration.java
@@ -0,0 +1,48 @@
+package com.stock.control.configuration;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan(basePackages="com.stock.control")
+@EnableWebMvc
+public class MvcConfiguration extends WebMvcConfigurerAdapter{
+
+	@Bean
+	public ViewResolver getViewResolver(){
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+	}
+
+	@Bean
+	public DataSource getDataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.postgresql.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:5432/stock-control");
+		dataSource.setUsername("postgres");
+		dataSource.setPassword("admin");
+		return dataSource;
+	}
+	
+	
+//	@Bean
+//	public ContactDAO getContactDAO() {
+//		return new ContactDAOImpl(getDataSource());
+//	}
+}
